baec06e18a870b863fa62d444af09bdd032af88b
Jay Fowler <jcafowler@gmail.com>
2020-02-11 19:24:49 -0500

    Fix syntax and order of code in schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,14 +1,17 @@
+DROP DATABASE IF EXISTS broken_project;
 CREATE DATABASE broken_project; 
 
-DROP DATABASE IF EXISTS broken_project;
+
+
+\c broken_project
 
 DROP TABLE IF EXISTS cars; 
-
-/c broken_project
-
-
-
 DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, 
+    username TEXT NOT NULL UNIQUE
+);
 
 CREATE TABLE cars (
     id SERIAL PRIMARY KEY, 
@@ -16,13 +19,7 @@ CREATE TABLE cars (
     model TEXT,
     year INTEGER,
     owner_id INT REFERENCES users(id) ON DELETE SET NULL
-)
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY, 
-    username TEXT NOT NULL UNIQUE
-)
-
+);
 
 
 
@@ -35,8 +32,8 @@ VALUES
 
 INSERT INTO cars(brand, model, year, owner_id)
 VALUES
-('Toyota', 'Camery', 1995, 1), 
-('Toyota', '4Runner', 1990, 1), 
+('Toyota', 'Camry', 1995, 1), 
+('Toyota', '4Runner', 1990, 1),
 ('Ford', 'Mustang', 1964, 2),
 ('Ford', 'Explorer', 2005, 3),
 ('Ford', 'Bronco', 2019, 3);
