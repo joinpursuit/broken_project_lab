29a5f46b76a06fcea579d7e9e43ba128ac3a3c02
John Jones Jr <jayjohnjones@gmail.com>
2020-02-12 10:36:57 -0500

    frontend working

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,14 @@
 
+    let allCars = document.querySelector("#allCars");
+    let select = document.querySelector("select");
+    let carsForOneUser = document.querySelector("#carsForOneUser");
 
-const fetchData = (url, cb, domEl) => {
+const fetchData = async (url, cb, domEl) => {
     try {
-        let res =  axios.get(url)
-        cb(res.data, domEl)
+        let res = await axios.get(url)
+        // debugger
+        cb(res.data.payload, domEl)
+        
     } catch (error) {
         console.log(error, " something went wrong")
     }
@@ -11,34 +16,33 @@ const fetchData = (url, cb, domEl) => {
 
 
 const fillUlWithCars = (data, domEl) => {
-    domEl.innerHTML = "";
-    data.cars.forEach(el => {
-        const li = document.createElement("li");
+    debugger
+    // domEl.innerHTML = "";
+    data.forEach(el => {
+        let li = document.createElement("li");
         li.innerText = `Brand: ${el.brand}, Model: ${el.model}, Year: ${el.year}`;
+        domEl.appendChild(li)
     })
-    domEl.appendChild(li)
 }
 
 
 const fillSelectBar =  (data, domEl) => {
     data.forEach(user => {
-        const option = document.createElement("option");
-        option.innerHTLM = user.name;
+        // debugger
+        let option = document.createElement("option");
+        option.innerHTML = user.username;
+        option.innerText = user.username;
         option.value = user.id;
         domEl.appendChild(option)
     })
 }
-
-
-document.addEventListener('DOMContentloaded', () => {
-    let allCars = document.querySelector(".allCars");
-    let select = document.querySelector("select");
-    let carsForOneUser = document.querySelector("#carsForOneUser");
     select.addEventListener("change", (e) => {
-        fetchData(`http://localhost:3000/users/${e.target.id}/cars`, fillUlWithCars, carsForOneUser)
+        debugger
+        let carsForOneUser = document.querySelector("#carsForOneuser");
+        fetchData(`http://localhost:3000/users/${e.target.value}/cars`, fillUlWithCars, carsForOneUser)
     })
     
     
-    fetchData("http:/localhost:3000/cars", fillUlWithCars, allCars)
-    fetchData("http://localhost:3000/users", fillselectBar, select);
-})
+    fetchData("http://localhost:3000/cars", fillUlWithCars, allCars);
+    fetchData("http://localhost:3000/users", fillSelectBar, select);
+
