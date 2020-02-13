463ed09b23b206ac2f60ca4b3f2c6bcef6f430f4
karenm09 <karenm929@hotmail.com>
2020-02-12 21:17:01 -0500

    update the port number to 3000 from 6000. change the forEach to loop through an array not an object and key intothe correct object

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express(); 
 app.use(cors());
 
-const PORT = 6000; 
+const PORT = 3000; 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
